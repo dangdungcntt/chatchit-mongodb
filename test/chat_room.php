0d93c797e6c0e050ea88c34f7b8278c86457bf36
SocolaDaiCa <tokentien@gmail.com>
2017-05-16 20:34:29 +0700

    them file danh sach phong

---
+++ test/chat_room.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<link rel="stylesheet" href="chat_room.css">
+	</head>
+	<body>
+	<?php for ($i=0; $i < 50; $i++) { ?>
+	
+		<a href="#">
+			<div class="chat_room">
+				<img src="http://avatar1.cctalk.vn/csmtalk_room/68" alt="">
+				<div class="crop">
+					<b title="Sing-Bolero 68 - TalkTV Idol">Sing-Bolero 68 - TalkTV Idol</b><br>
+					<span class="text-muted" title="Kênh viết tí cho nó dài dài ra dài thật dài">Kênh viết tí cho nó dài dài ra dài thật dài</span>
+				</div>
+			</div>
+		</a>
+	<?php } ?>
+	</body>
+</html>
