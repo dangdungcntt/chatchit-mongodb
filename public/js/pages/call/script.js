db6ba86b16e64f31858e94234cad2835e2ba7f57
DangDung <dangdungcntt@gmail.com>
2017-09-11 22:33:15 +0700

    use xirsys turn server

--- public/js/pages/call/script.js
+++ public/js/pages/call/script.js
@@ -2,22 +2,22 @@ let hostname = `${location.protocol}//${location.host}`;
 let socket = io(`${hostname}/call`); // jshint ignore:line
 
 let myid; 
-// let customConfig;
+let customConfig;
 
 // // Call Xirsys ICE servers
-// $.ajax ({
-//   url: "https://global.xirsys.net/_turn/ChatChit/",
-//   type: "PUT",
-//   async: false,
-//   headers: {
-//     "Authorization": "Basic " + btoa("dangdungcntt:2655cd46-96a5-11e7-8791-9354bcb0cf6a")
-//   },
-//   success: function (res){
-//     console.log("ICE List: "+res.v.iceServers);
-//     customConfig = res.v;
-//     console.log(customConfig);
-//   }
-// });
+$.ajax ({
+  url: "https://global.xirsys.net/_turn/ChatChit/",
+  type: "PUT",
+  async: false,
+  headers: {
+    "Authorization": "Basic " + btoa("dangdungcntt:2655cd46-96a5-11e7-8791-9354bcb0cf6a")
+  },
+  success: function (res){
+    console.log("ICE List: "+res.v.iceServers);
+    customConfig = res.v;
+    console.log(customConfig);
+  }
+});
 
 
 function openStream() {
@@ -39,9 +39,5 @@ const peer = new Peer({
     host: 'dangdung-peer.herokuapp.com', 
     secure: true, 
     port: 443, 
-    config: {
-      'iceServers': [
-          {url:'stun:stun.l.google.com:19302'}
-      ]
-    }
+    config: customConfig
 });
