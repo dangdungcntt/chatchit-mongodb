aa876d12e3027b664a097f8d4df227650c53abc7
DangDung <dangdungcntt@gmail.com>
2018-02-02 17:11:02 +0000

    add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'CHATCHIT',
+      script    : 'index.js',
+      env: {
+        COMMON_VARIABLE: 'true'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'dangdungcntt
+      host : '35.186.159.152',
+      ref  : 'origin/production
+      repo : 'git@github.com:dangdungcntt/chatchit-mongodb.git',
+      path : '/home/dangdungcntt/chatchit',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
+    },
+  }
+};
